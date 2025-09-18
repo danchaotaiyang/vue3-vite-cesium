0038109352accb56654333aced3798bb0f8f72cf
danchaotaiyang <yuxiaolong00@qq.com>
2025-09-18 15:55:13 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import cesium from 'vite-plugin-cesium';
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
